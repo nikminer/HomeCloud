ed1bd4f5ea1095d624c8cfeb93e06ab833a462be
nikminer <nikminerfeed@gmail.com>
2018-12-10 14:21:45 +0300

    v 0.5.1

--- File/views.py
+++ File/views.py
@@ -7,30 +7,34 @@ import shutil
 import os
 import time
 
-def convertBytes(byte):
-    if byte>1024 and byte<1048575:
-        return str(round(byte/1024,3))+ " KB"
-    elif byte>1048576 and byte<1073741823:
-        return str(round(byte/1048576,3))+ " MB"
-    elif byte>1073741824 and byte<1099511627775:
-        return str(round(byte/1073741824,3)) + " GB"
-    elif byte>1099511627776:
-        return str(round(byte/1099511627776))+" PB"
-    return str(byte) + " Bytes"
-
 def view1(request,path):    
     images=['WEBP','JPEG','SVG','PNG','GIF','JPG']
-
+    texts= ['TXT','MB','LOG','PY']
     if os.path.basename(path).split(".")[-1].upper() in images:
         return viewImage(request,path)
+    elif os.path.basename(path).split(".")[-1].upper() in texts:
+        return viewText(request,path)
     else:
         return back(request,path)
+
+def viewText(request, path):
+    text = open(path,"r").read()
+    return render(request, "File/TextFilePreview.html", 
+    {       
+        "filename":os.path.basename(path),
+        "path":os.path.split(path)[0],
+        "time":time.ctime(os.path.getctime(path)),
+        "size":os.path.getsize(path),
+        "text":text,
+        "host":"http://"+request.get_host(),
+        "pathes":getPathHierrarhyFile(path)
+    })
 
 def viewImage(request, path):
     image = open(path,"rb").read()
     base=str(base64.encodestring(image),"utf-8")
     
-    return render(request, "File/filePreview.html", 
+    return render(request, "File/ImageFilePreview.html", 
     {       
         "filename":os.path.basename(path),
         "path":os.path.split(path)[0],
