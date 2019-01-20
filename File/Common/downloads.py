f5be6fa5e4ee137124a679381987fd86793e82c3
nikminer <nikminerfeed@gmail.com>
2019-01-20 18:12:52 +0300

    v0.5.7

---
+++ File/Common/downloads.py
@@ -0,0 +1,47 @@
+import os
+import zipfile
+from django.http import StreamingHttpResponse
+
+def downloadFile(request, path):
+    
+    def file_iterator(file_name, chunk_size=512):
+        with open(file_name,'rb') as f:
+            while True:
+                chunk = f.read(chunk_size)
+                if chunk:
+                    yield chunk
+                else:
+                    break
+
+    response = StreamingHttpResponse(file_iterator(path))
+    response['Content-Type'] = 'application/octet-stream'
+    response['Content-Disposition'] = 'attachment;filename="{0}"'.format(os.path.split(path)[1])
+    return response
+
+def downloadFolder(request,path):
+    pathes=os.path.split(path)
+
+    zipf = zipfile.ZipFile(path+"/"+pathes[1]+".zip", 'w', zipfile.ZIP_DEFLATED)
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if(file != pathes[1]+".zip"):
+                absfn=os.path.join(root, file)
+                zfn = absfn[len(path)+len(os.sep):]
+                zipf.write(absfn,zfn)
+    zipf.close()
+
+    def dir_iterator(file_name, chunk_size=512):
+        with open(file_name,'rb') as f:
+            while True:
+                chunk = f.read(chunk_size)
+                if chunk:
+                    yield chunk
+                else:
+                    f.close()
+                    os.remove(file_name)
+                    break
+
+    response = StreamingHttpResponse(dir_iterator(path+"/"+pathes[1]+".zip"))
+    response['Content-Type'] = 'application/octet-stream'
+    response['Content-Disposition'] = 'attachment;filename="{0}"'.format(pathes[1]+".zip")
+    return response
--- File/urls.py
+++ File/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, re_path
 from . import views
 from .Common.movment import back, explorer
 from .Common.actions import deletedir, createdir, movedir, deletefile
+from .Common.downloads import downloadFile,downloadFolder
 from .Common.Preview import ViewSwitcher
 
 urlpatterns = [
@@ -11,6 +12,7 @@ urlpatterns = [
     path('deldir<path:path>', deletedir),
     path('mkdir<path:path>', createdir),
     path('movedir<path:path>+<path:path1>', movedir),
-    path('downloadfile<path:path>', views.downloadFile),
-    path('delfile<path:path>',deletefile)
+    path('downloadfile<path:path>', downloadFile),
+    path('delfile<path:path>',deletefile),
+    path('downloadfolder<path:path>', downloadFolder)
 ]
