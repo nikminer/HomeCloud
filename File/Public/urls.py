c8cd345b65d3971515114b556f092131149e3aba
nikminer <nikminerfeed@gmail.com>
2019-03-24 18:28:37 +0300

    FileTemplates

--- File/Public/urls.py
+++ File/Public/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .actions import Shering,explorePublic,exist,AddFile,download
+from File.Public import actions
 
 urlpatterns = [
-    path('',explorePublic),
-    path('shering<path:path>', Shering),
-    path('existpseudo',exist),
-    path('shere',AddFile),
-    path('<str:name>',download)
+    path('', actions.explorePublic),
+    path('sharing<path:path>', actions.Shering),
+    path('existpseudo',actions.exist),
+    path('share/addfile',actions.AddFile),
+    path('<str:name>',actions.download)
 ]
--- File/models.py
+++ File/models.py
@@ -3,6 +3,6 @@ from django.db import models
 class Publicfile(models.Model):
     pseudoname=models.CharField(max_length=255,unique=True,primary_key=True)
     Sharinguser=models.CharField(max_length=255,default='')
-    path=models.FilePathField(max_length=1000)
-    isvisible=models.CharField(max_length=3,default="off")
+    path=models.FilePathField(max_length=100000)
+    isvisible=models.CharField(max_length=5,default="false")
     isdownloaded=models.CharField(max_length=3,default="on")
